ec12d60483078898260e6a62b4b5a29eddca0de7
Cansu Beşler <83400245+cansu5lerr@users.noreply.github.com>
2024-01-08 01:29:43 +0300

    Update ImageRepository.java

--- src/main/java/com/example/mekan54/repository/ImageRepository.java
+++ src/main/java/com/example/mekan54/repository/ImageRepository.java
@@ -4,9 +4,16 @@ import com.example.mekan54.model.Image;
 import com.example.mekan54.model.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
-    void deleteAllByVenue(Venue venue);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Image i WHERE i.venue.id = :venueId")
+    void deleteImagesByVenueId(@Param("venueId") Long venueId);
 
 }
