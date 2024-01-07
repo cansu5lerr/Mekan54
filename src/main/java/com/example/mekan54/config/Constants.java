6ac17afc6a03c124eb93eb150d8ce9532393a7bb
Cansu Beşler <83400245+cansu5lerr@users.noreply.github.com>
2024-01-07 16:30:25 +0300

    Update Constants.java

--- src/main/java/com/example/mekan54/config/Constants.java
+++ src/main/java/com/example/mekan54/config/Constants.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Constants {
-    public static final String FIREBASE_SDK_JSON = "com\\example\\mekan54\\config\\serviceAccountKey.json";
+    public static final String FIREBASE_SDK_JSON = "src/main/java/com/example/mekan54/config/serviceAccountKey.json";
     public static final String FIREBASE_BUCKET = "mekan54-badfd.appspot.com";//enter your bucket name
     public static final String FIREBASE_PROJECT_ID ="mekan54-badfd";//enter your project id
 }
