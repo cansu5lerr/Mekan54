be69573059b610f82ebe221048002d4e06d0409a
Cansu Beşler <83400245+cansu5lerr@users.noreply.github.com>
2024-01-09 23:06:54 +0300

    Update UserDetailsServiceImpl.java

--- src/main/java/com/example/mekan54/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/mekan54/security/service/UserDetailsServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.mekan54.security.service;
 
+import com.example.mekan54.model.Favorite;
 import com.example.mekan54.model.User;
+import com.example.mekan54.model.Image;
+
+import com.example.mekan54.model.Venue;
 import com.example.mekan54.repository.UserRepository;
 import com.example.mekan54.security.jwt.EmailPasswordAuthenticationToken;
 import com.example.mekan54.security.jwt.JwtUtils;
@@ -14,8 +18,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.mekan54.payload.response.UserDetailsResponse;
-import java.util.Optional;
 
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -34,13 +39,37 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     public ResponseEntity<?> getUserProfile (String token) {
         User authenticatedUser = getAuthenticatedUserFromToken(token);
+        UserDetailsResponse userDetailsResponse = new UserDetailsResponse();
         if (authenticatedUser instanceof User) {
-
-            return ResponseEntity.ok(new UserDetailsResponse(authenticatedUser.getName(),
-                    authenticatedUser.getSurname(),
-                    authenticatedUser.getEmail()));
+            List<Favorite> FavoriteList = authenticatedUser.getFavorites();
+            List<Map<String, Object>> venuesResponseList = new ArrayList<>();
+            List<List<String>> imageUrlList = new ArrayList<>();
+            for (Favorite favorite : FavoriteList) {
+                Venue venue = favorite.getVenue();
+                Map<String, Object> venueList = new HashMap<>();
+                venueList.put("name", Objects.toString(venue.getVenueName(), "null"));
+                venueList.put("category", Objects.toString(venue.getCategory().getCategoryName(), "null"));
+                venueList.put("address", Objects.toString(venue.getAdress(), "null"));
+                venueList.put("website", Objects.toString(venue.getWebsite(), "null"));
+                venueList.put("workingHour", Objects.toString(venue.getWorkingHour(), "null"));
+                List<Image> imageList = venue.getImages();
+                List<String> imgList = new ArrayList<>();
+               for(Image image : imageList) {
+                   imgList.add(image.getImgUrl());
+               }
+                venueList.put("imageUrl", imgList);
+                imageUrlList.add(imgList);
+                venuesResponseList.add(venueList);
+            }
+            userDetailsResponse.setVenuesResponseList(venuesResponseList);
+            userDetailsResponse.setEmail(authenticatedUser.getEmail());
+            userDetailsResponse.setName(authenticatedUser.getName());
+            userDetailsResponse.setSurname(authenticatedUser.getSurname());
+            return ResponseEntity.ok().body(userDetailsResponse);
         }
-        return ResponseEntity.badRequest().body("Kullanıcı bulunamadı");
+        Map<String , String> responseMap = new HashMap<>();
+        responseMap.put("message", "Kullanıcı bulunamadı.");
+        return ResponseEntity.badRequest().body(responseMap);
     }
 
     //User güncellemesi
