ff67062d9c25801740ddc537c215955d78a3df1d
Cansu Beşler <83400245+cansu5lerr@users.noreply.github.com>
2024-01-10 17:06:42 +0300

    Update ImageService.java

--- src/main/java/com/example/mekan54/security/service/ImageService.java
+++ src/main/java/com/example/mekan54/security/service/ImageService.java
@@ -40,6 +40,8 @@ public class ImageService {
     VenueRepository venueRepository;
   @Autowired
     UserRepository userRepository;
+    @Value("${spring.servlet.multipart.max-request-size}") // application.properties veya application.yml'den okunan max dosya boyutu
+    private long maxFileSize;
     @Transactional
     public ResponseEntity<?> uploadImages(String token, List<MultipartFile> multipartFiles, List<String> objectNames) throws IOException {
         User authenticatedUser = userDetailsService.getAuthenticatedUserFromToken(token);
@@ -51,7 +53,11 @@ public class ImageService {
             for (int i = 0; i < multipartFiles.size(); i++) {
                 MultipartFile multipartFile = multipartFiles.get(i);
                 String objectName = objectNames.get(i);
-
+                if (multipartFile.getSize() > maxFileSize) {
+                    Map<String, String> responseMap = new HashMap<>();
+                    responseMap.put("message", "Dosya boyutu sınırları aşıyor.");
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
+                }
                 FileInputStream serviceAccount = new FileInputStream(FIREBASE_SDK_JSON);
                 File file = convertMultiPartToFile(multipartFile);
                 Path filePath = file.toPath();
@@ -106,6 +112,12 @@ public class ImageService {
             if((authenticatedUser.getProfileImage()) != null) {
 
                 deleteImageUser(token);
+            }
+
+            if (multipartFile.getSize() > maxFileSize) {
+                Map<String, String> responseMap = new HashMap<>();
+                responseMap.put("message", "Dosya boyutu sınırları aşıyor.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
             }
             FileInputStream serviceAccount = new FileInputStream(FIREBASE_SDK_JSON);
             File file = convertMultiPartToFile(multipartFile);
